e516a28edf6f0940de1545c5544b6e0aaffe39d1
Pedro Josue <pedro.josue.ap@outlook.com>
2025-02-13 22:21:09 -0300

    Primeiro exercicio, envolve o uso do scanner, declaração de variaveis e concatenação.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String nome, endereco;
+        int idade;
+        System.out.println("Digite o seu nome");
+        nome = scanner.nextLine();
+        System.out.println("Digite a sua idade");
+        idade = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite o seu endereço");
+        endereco = scanner.nextLine();
+        System.out.println("Olá " + nome + " que tem " + idade + " anos e mora em " + endereco);
+        scanner.close();
     }
 }
